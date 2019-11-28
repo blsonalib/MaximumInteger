6790b8739f876538f96f45cf266fdc7e669d1b77
blsonalib <blsonalib@gmail.com>
2019-11-28 18:06:00 +0530

    Refactor all the three to one generic method and find the maximum

---
+++ src/main/java/com/maximuminteger/MaximumTestGeneric.java
@@ -0,0 +1,19 @@
+package com.maximuminteger;
+
+public class MaximumTest
+{
+    public static <T extends Comparable<T>>T  toMaximum(T num1,T num2,T num3){
+        T maximum=num1;
+        if(num2.compareTo(maximum)>0)
+        {
+            maximum=num2;
+        }
+        if(num3.compareTo(maximum)>0)
+        {
+            maximum=num3;
+        }
+        return maximum;
+
+    }
+
+}
