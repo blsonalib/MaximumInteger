73729b0d5d7d3db7a55699137a5dd176ce376ba3
blsonalib <blsonalib@gmail.com>
2019-11-28 19:09:02 +0530

    Refactor to create generic class to take in three variables of generic type

--- src/main/java/com/maximuminteger/MaximumTestGeneric.java
+++ src/main/java/com/maximuminteger/MaximumTestGeneric.java
@@ -1,8 +1,18 @@
 package com.maximuminteger;
 
-public class MaximumTestGeneric
+public class MaximumTestGeneric<T extends Comparable>
 {
-    public static <T extends Comparable<T>>T  toMaximum(T num1,T num2,T num3){
+    private T num1;
+    private T num2;
+    private T num3;
+
+    public MaximumTestGeneric(T num1, T num2, T num3) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+    }
+
+    public static  <T extends Comparable<T>> T toMaximum(T num1, T num2, T num3){
         T maximum=num1;
         if(num2.compareTo(maximum)>0)
         {
@@ -14,4 +24,8 @@ public class MaximumTestGeneric
         }
         return maximum;
     }
+    public <T extends Comparable<T>> T toMaximum(){
+    T maximum= (T) MaximumTestGeneric.toMaximum(num1,num2,num3);
+    return maximum;
+}
 }
--- src/test/java/com/maximuminteger/TestMaximum.java
+++ src/test/java/com/maximuminteger/TestMaximum.java
@@ -4,22 +4,21 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestMaximum {
+
     @Test
     public void givenMessage_CheckToThreeInteger_ReturnMaximum(){
-        MaximumTestGeneric maximumTest=new MaximumTestGeneric();
-        Integer max=maximumTest.toMaximum(40,20,30);
-        Assert.assertEquals(40,max.intValue());
+        Integer max = MaximumTestGeneric.toMaximum(40, 20, 30);
+        Assert.assertEquals((Integer) 40,max);
     }
     @Test
     public void givenMessage_CheckToThreeFloat_ReturnMaximum(){
-        MaximumTestGeneric maximumTest=new MaximumTestGeneric();
-        Float max=maximumTest.toMaximum(5.6f,4.2f,8.9f);
-        Assert.assertEquals(8.9,max.floatValue(),0.01);
+        Float max=MaximumTestGeneric.toMaximum(6.7f,8.9f,3.2f);
+        Assert.assertEquals((Float)8.9f,max);
     }
     @Test
     public void givenMessage_CheckToThreeString_ReturnMaximum(){
-        MaximumTestGeneric maximumTest=new MaximumTestGeneric();
-        String max=maximumTest.toMaximum("Sona","Adity","Priya");
-        Assert.assertEquals("Sona",max);
+
+        String max=MaximumTestGeneric.toMaximum("Sona","Adity","Priya");
+        Assert.assertEquals((String) "Sona",max);
     }
 }
