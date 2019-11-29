1a763dc32fb1a02cedab6a8996cd5f3df1dcae81
blsonalib <blsonalib@gmail.com>
2019-11-29 13:21:19 +0530

    Add :to print maximum value using generics

--- src/main/java/com/maximuminteger/MaximumTestGeneric.java
+++ src/main/java/com/maximuminteger/MaximumTestGeneric.java
@@ -6,6 +6,13 @@ public class MaximumTestGeneric<T extends Comparable>
 {
     public static <T extends Comparable<T>> T toMaximum(T... array) {
         Arrays.sort(array);
-        return array[array.length - 1];
+        T max= array[array.length - 1];
+        MaximumTestGeneric.printMax(max);
+        return max;
     }
+
+    private static <T extends Comparable<T>> void  printMax(T max) {
+        System.out.println("Maximum Value:"+max);
+    }
+
 }
